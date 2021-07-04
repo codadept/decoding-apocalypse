0066874aa79af0ca4743fa494948d6c3a16aa8b3
Pratik Majumdar(codadept) <info.pratikmajumdar@gmail.com>
2021-07-05 00:59:44 +0530

    initial boilerplate setup

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # decoding-apocalypse
 HackRx - Annual Hackathon hosted by Bajaj Finserv
+
+
+### clone the repo
+```bash
+git clone git@github.com:codadept/decoding-apocalypse.git
+cd decoding-apocalypse/
+```
+
+### Start the webapp
+```bash
+npm i
+npm start
+```
+
+###
+```
+visit: localhost:3000/ in your web browser
+```
---
+++ index.js
@@ -0,0 +1,22 @@
+const   express     = require('express'),
+        app         = express(),
+        path        = require('path'),
+        PORT        = process.env.PORT || 3000;
+
+
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('*', (req,res) => {
+    res.send('Error');
+})
+
+app.listen(PORT, () => {
+    console.log(`THE SERVER IS RUNNING ON PORT ${PORT}`);
+})
