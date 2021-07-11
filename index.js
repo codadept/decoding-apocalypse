9db7238f686efb9c9c3c6961d3cb1df77c37a38e
Vikas <vikassuthar528@gmail.com>
2021-07-11 18:39:09 +0530

    added backend authentication

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const   express     = require('express'),
         path        = require('path'),
         PORT        = process.env.PORT || 3000;
 
+require('./db/conn');
+const   Register    = require('./models/signup');
 
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -29,6 +33,33 @@ app.get('/login', (req, res)=> {
 
 app.get('/signup', (req, res) => {
     res.render('signup');
+});
+
+app.post('/signup', async (req, res) => {
+    try {
+        const password = req.body.password;
+        const cpassword = req.body.confirmpassword;
+        if(password === cpassword) {
+            const registerEmployee = new Register({
+                username: req.body.username,
+                email: req.body.email,
+                age: req.body.age,
+                password: req.body.password,
+                confirmpassword: req.body.confirmpassword
+            });
+
+            const registered = await registerEmployee.save();
+            res.status(201).render('login');
+        }
+        else {
+            res.send(`password didn't match`);
+        }
+        // console.log(req.body.username);
+        // res.send(req.body.username);
+    }
+    catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 app.get('*', (req,res) => {
---
+++ models/signup.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    confirmpassword:{
+        type: String,
+        required: true
+    }
+});
+
+const Register = new mongoose.model('Register', employeeSchema);
+module.exports = Register;
